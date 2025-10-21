c187eedb673abc3b5f394011ca6d72724000cfbb
MGComhard <MGuenther@o365.comhard.de>
2025-10-21 20:52:47 +0000

    Final 3.Tag

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken';
 
 export function authMiddleware(req, res, next) {
   const token = req.cookies.token;
-  if (!token) return res.status(401).send('Nicht eingeloggt');
+  if (!token) 
+    return res.status(401).send('Nicht eingeloggt');
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
